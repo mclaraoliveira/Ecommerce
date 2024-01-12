25005dbf4b5da80cf1dcd0b39132e83bd2ad22ad
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2024-01-12 12:21:30 -0300

    Acrescentei os produtos mais vendidos

--- frontend/ecommerce/src/Pages/Loja.jsx
+++ frontend/ecommerce/src/Pages/Loja.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
 
 export const Loja = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
     </div>
   )
 }
