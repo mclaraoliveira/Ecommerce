a91e0adfe5bbb1a3eba76652c349bd36a8929b00
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2024-01-17 19:05:46 -0300

    Página de Login pronta

--- frontend/ecommerce/src/Pages/LoginSignup.jsx
+++ frontend/ecommerce/src/Pages/LoginSignup.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Cadastre-se</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Seu nome' name="" id="" />
+          <input type="email" placeholder='E-mail' />
+          <input type="password" placeholder='Senha' name="" id="" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Já possui uma conta? <span>Entre aqui</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>Ao continuar, eu aceito os Termos de Uso e a Política de Privacidade.</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
